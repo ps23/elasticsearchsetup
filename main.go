c600cbe008b5bdd33d48f63084600475c4415d6a
Patrick Seidler <p.seidler@mdx.ac.uk>
2018-01-05 12:45:03 +0100

    Use main.go flags instead of const defs

--- main.go
+++ main.go
@@ -3,18 +3,42 @@ package main
 import (
     "fmt"
     "os"
+    "flag"
+    "log"
     "./elasticsearchsetup"
 )
 
-const url = "http://localhost:9200"
-const sniff = false
-const index = "elastic-test"
-const mapping = "mapping.json"
-const field = "mo_notes"
+const url_c = "http://localhost:9200"
+const sniff_c = false
+const index_c = "elastic-test"
+const mapping_c = "mapping.json"
+const field_c = "mo_notes"
 
 func main() {
-    client := elasticsearchsetup.SetupTestClient(url, sniff)
-    elasticsearchsetup.SetupTestClientAndCreateIndexAndAddDocs(client, index, mapping, field, true)
+  var (
+    url     = flag.String("url", url_c, "Elasticsearch host.")
+    sniff     = flag.Bool("sniff", sniff_c, "Sniff elasticsearch nodes")
+    index   = flag.String("index", index_c, "Index name.")
+    mapping   = flag.String("mapping", mapping_c, "Mapping file.")
+    field   = flag.String("field", field_c, "Field name for text search tests.")
+  )
+  flag.Parse()
+
+  if *url == "" {
+    log.Fatal("missing url")
+  }
+  if *index == "" {
+    log.Fatal("missing index name")
+  }
+  if *mapping == "" {
+    log.Fatal("missing mapping file definition")
+  }
+  if *field == "" {
+    log.Fatal("missing text search field definition")
+  }
+
+  client := elasticsearchsetup.SetupTestClient(*url, *sniff)
+  elasticsearchsetup.SetupTestClientAndCreateIndexAndAddDocs(client, *index, *mapping, *field, true)
 }
 
 func check(err error) {
